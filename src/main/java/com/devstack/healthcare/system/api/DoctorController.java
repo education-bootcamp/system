666042a8946b7c205488cfb0c4069ab3f685add0
ozen-hub <info.ozenhub@gmail.com>
2023-09-25 21:17:48 +0530

    2

--- src/main/java/com/devstack/healthcare/system/api/DoctorController.java
+++ src/main/java/com/devstack/healthcare/system/api/DoctorController.java
@@ -1,13 +1,14 @@
 package com.devstack.healthcare.system.api;
 
+import com.devstack.healthcare.system.dto.request.RequestDoctorDto;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/doctors")
 public class DoctorController {
     @PostMapping
-    public String createDoctor(){
-        return "createDoctor";
+    public String createDoctor(@RequestBody RequestDoctorDto doctorDto){
+        return doctorDto.toString();
     }
 
     @GetMapping
