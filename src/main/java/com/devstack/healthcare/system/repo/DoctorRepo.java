61234873816c06430e6d83bb1ff96e50e407352c
ozen-hub <info.ozenhub@gmail.com>
2023-09-25 22:00:25 +0530

    6

--- src/main/java/com/devstack/healthcare/system/repo/DoctorRepo.java
+++ src/main/java/com/devstack/healthcare/system/repo/DoctorRepo.java
@@ -2,6 +2,10 @@ package com.devstack.healthcare.system.repo;
 
 import com.devstack.healthcare.system.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@EnableJpaRepositories
 public interface DoctorRepo extends JpaRepository<Doctor,Long> {
 }
---
+++ src/main/java/com/devstack/healthcare/system/service/impl/DoctorServiceImpl.java
@@ -0,0 +1,52 @@
+package com.devstack.healthcare.system.service.impl;
+
+import com.devstack.healthcare.system.dto.request.RequestDoctorDto;
+import com.devstack.healthcare.system.dto.response.ResponseDoctorDto;
+import com.devstack.healthcare.system.entity.Doctor;
+import com.devstack.healthcare.system.repo.DoctorRepo;
+import com.devstack.healthcare.system.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class DoctorServiceImpl implements DoctorService {
+
+    private final DoctorRepo doctorRepo;
+
+    @Autowired
+    public DoctorServiceImpl(DoctorRepo doctorRepo) {
+        this.doctorRepo = doctorRepo;
+    }
+
+    @Override
+    public void createDoctor(RequestDoctorDto dto) {
+
+        Doctor doctor =new Doctor(
+                "", dto.getName(), dto.getAddress(),
+                dto.getContact(), dto.getSalary()
+        );
+
+        doctorRepo.save(doctor);
+    }
+
+    @Override
+    public ResponseDoctorDto getDoctor(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteDoctor(long id) {
+
+    }
+
+    @Override
+    public void updateDoctor(long id, RequestDoctorDto dto) {
+
+    }
+
+    @Override
+    public List<ResponseDoctorDto> getAllDoctors(String searchText, int page, int size) {
+        return null;
+    }
+}
