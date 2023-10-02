8e6d9bf7a92182d84d83d32a5b2dd55c4ad91226
ozen-hub <info.ozenhub@gmail.com>
2023-10-02 20:57:22 +0530

    19

---
+++ src/main/java/com/devstack/healthcare/system/security/ApplicationUserRole.java
@@ -0,0 +1,9 @@
+package com.devstack.healthcare.system.security;
+
+import java.util.Set;
+
+public enum ApplicationUserRole {
+    DOCTOR,
+    ADMIN
+    private final Set<ApplicationUserPermission>
+}
