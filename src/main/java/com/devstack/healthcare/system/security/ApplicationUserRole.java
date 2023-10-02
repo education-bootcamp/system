c523ff6365c9f3e73f95444cb1bcf563d944a6b6
ozen-hub <info.ozenhub@gmail.com>
2023-10-02 21:10:35 +0530

    20

--- src/main/java/com/devstack/healthcare/system/security/ApplicationUserRole.java
+++ src/main/java/com/devstack/healthcare/system/security/ApplicationUserRole.java
@@ -1,9 +1,30 @@
 package com.devstack.healthcare.system.security;
 
+import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+
+
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum ApplicationUserRole {
-    DOCTOR,
-    ADMIN
-    private final Set<ApplicationUserPermission>
+    DOCTOR(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet());
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+    public Set<ApplicationUserPermission> getPermissions(){
+        return permissions;
+    }
+    public Set<SimpleGrantedAuthority> getSimpleGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions= getPermissions()
+                .stream().map(permission->new
+                        SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+        return permissions;
+    }
 }
