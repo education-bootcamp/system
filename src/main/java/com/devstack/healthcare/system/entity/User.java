359281b36ac1b72686918008bcfe0affe64d9f5a
ozen-hub <info.ozenhub@gmail.com>
2023-10-02 20:34:01 +0530

    18

---
+++ src/main/java/com/devstack/healthcare/system/entity/User.java
@@ -0,0 +1,30 @@
+package com.devstack.healthcare.system.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class User {
+    @Id
+    private long id;
+    private String fullName;
+    private String email;
+    private String password;
+    private boolean isAccountNonExpired;
+    private boolean isCredentialsNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isEnabled;
+
+    @OneToMany(mappedBy = "user")
+    private Set<UserRoleHasUser> userRoleHasUsers;
+
+}
---
+++ src/main/java/com/devstack/healthcare/system/entity/UserRoleHasUser.java
@@ -0,0 +1,28 @@
+package com.devstack.healthcare.system.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserRoleHasUser {
+    @EmbeddedId
+    private UserRoleHasUserKey id= new UserRoleHasUserKey();
+
+    @ManyToOne
+    @MapsId("user")
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @ManyToOne
+    @MapsId("userRole")
+    @JoinColumn(name = "role_id",nullable = false)
+    private UserRole userRole;
+}
