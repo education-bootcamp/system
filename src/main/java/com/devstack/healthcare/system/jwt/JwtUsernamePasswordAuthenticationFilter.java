c0995b3df989a96415107d542aa262954a24bdb2
education-bootcamp <education.seekerscloud@gmail.com>
2023-10-09 22:10:25 +0530

    3

---
+++ src/main/java/com/devstack/healthcare/system/jwt/JwtUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package com.devstack.healthcare.system.jwt;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.crypto.SecretKey;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtUsernamePasswordAuthenticationFilter
+        extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtConfig jwtConfig;
+    private final SecretKey secretKey;
+
+    public JwtUsernamePasswordAuthenticationFilter(AuthenticationManager authenticationManager, JwtConfig jwtConfig, SecretKey secretKey) {
+        this.authenticationManager = authenticationManager;
+        this.jwtConfig = jwtConfig;
+        this.secretKey = secretKey;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+
+        try{
+            UsernamePasswordAuthenticationRequest
+                    usernamePasswordAuthenticationRequest =
+                    new ObjectMapper().readValue(
+                            request.getInputStream(),
+                    UsernamePasswordAuthenticationRequest.class);
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
