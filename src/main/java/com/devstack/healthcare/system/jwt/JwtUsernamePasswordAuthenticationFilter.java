eb8cc63cd92ab6a1c4005b3de499952144d8ec01
education-bootcamp <education.seekerscloud@gmail.com>
2023-10-09 22:52:12 +0530

    5

--- src/main/java/com/devstack/healthcare/system/jwt/JwtUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/devstack/healthcare/system/jwt/JwtUsernamePasswordAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.devstack.healthcare.system.jwt;
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -10,9 +11,13 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class JwtUsernamePasswordAuthenticationFilter
         extends UsernamePasswordAuthenticationFilter {
@@ -49,5 +54,22 @@ public class JwtUsernamePasswordAuthenticationFilter
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(
+                        LocalDate.now().plusDays(jwtConfig.getTokenExpirationAfterDays())))
+                .signWith(secretKey)
+                .compact();
+        response.addHeader(jwtConfig.getAuthorizationHeader(),
+                jwtConfig.getTokenPrefix()+token);
     }
 }
